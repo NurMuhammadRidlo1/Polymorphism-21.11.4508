7440def85de5aae11048b17e217d6a47008669f8
Nur Muhammad Ridlo <nurmuhammadridlo@gmail.com>
2022-06-10 09:39:19 +0700

    First commit

---
+++ Polymorphism-21.11.4508/PrinterWindows.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_21._11._4508
+{
+    public class PrinterWindows
+    {
+        public virtual void Show()
+        {
+            Console.WriteLine("Printer display dimension: default*default");
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine("Default Printer Printing.....");        }
+        }
+}
